7002b3719cc38f103e60ad023fb2807209b5e1d4
Alex Kost <alexkost819@gmail.com>
2018-01-20 15:35:43 -0800

    use_tensorboard functionality added and general cleanup.

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 """Created on 24 June 2017.
 @author: Alex Kost
-@description: mastermind train script for model
+@description: Training class for CNN and RNN models
 
 Attributes:
     DEFAULT_FORMAT (str): Logging format
@@ -20,13 +20,13 @@ import tensorflow as tf
 
 # Alex Python
 from data_processor import DataProcessor
-from rnn_model import LSTMModel    # RNN MODEL
+from rnn_model import RNNModel     # RNN MODEL
 from cnn_model import CNNModel     # CNN MODEL
 
 # Progressbar config
 progressbar.streams.wrap_stderr()
 
-# Logging constants
+# Constants
 DEFAULT_FORMAT = '%(asctime)s: %(levelname)s: %(message)s'
 LOGFILE_NAME = 'train.log'
 OUTPUT_DIR = 'output'
@@ -46,57 +46,75 @@ class TrainModel(DataProcessor):
         n_epochs (int): number of times we go through all data
         summary_op (TensorFlow operation): summary operation of all tf.summary objects
     """
-
-    def __init__(self, model, n_epochs=20, batch_size=32, learning_rate=.00005, dropout_rate=0.5):
+    def __init__(self, model, n_epochs=20, batch_size=32):
         """Constructor.
 
         Args:
             model (TensorFlow model object): Model to train and evaluate
-            n_epochs (int): number of times we go through all data
-            batch_size (int): number of examples in a single batch
-            learning_rate (float): learning rate, used for optimizing
-            dropout_rate (float): dropout rate; 0.1 == 10% of input units drop out
+            n_epochs (int, optional): number of times we go through all data
+            batch_size (int, optional): number of examples in a single batch
         """
         # TRAINING PARAMETERS
         self.n_epochs = n_epochs
         self.batch_size = batch_size
-        self.learning_rate = learning_rate
-        self.dropout_rate = dropout_rate
 
         # CONSTANT
-        self.model = model(self.learning_rate, self.dropout_rate)
-        self.model.build_model()
+        self.model = model
         self.summary_op = None
         self.logger = logging.getLogger(__name__)
-        self.n_checks = 10
+        self.n_checks = 5
 
         # INPUT DATA/LABELS
         super(TrainModel, self).__init__(self.model.n_classes, self.model.n_features)
         self.preprocess_data_by_label()
 
         # HELPER VARIABLES
+        self._ex_per_epoch = None
+        self._steps_per_epoch = None
+        self._train_length_ex = None
+        self._train_length_steps = None
+        self.calculate_helpers()
+
+    def calculate_helpers(self):
+        """Calculate helper variables for training length."""
         self._ex_per_epoch = len(self.train_files)
         self._steps_per_epoch = int(ceil(self._ex_per_epoch / float(self.batch_size)))
         self._train_length_ex = self._ex_per_epoch * self.n_epochs
         self._train_length_steps = self._steps_per_epoch * self.n_epochs
 
-    def train_model(self):
-        """Train the model."""
+        self.logger.debug('self._ex_per_epoch: %d', self._ex_per_epoch)
+        self.logger.debug('self._steps_per_epoch: %d', self._steps_per_epoch)
+        self.logger.debug('self._train_length_ex: %d', self._train_length_ex)
+        self.logger.debug('self._train_length_steps: %d', self._train_length_steps)
+
+    def train_model(self, use_tensorboard=True):
+        """Train the model.
+
+        Args:
+            use_tensorboard (bool, optional): Description
+
+        Returns:
+            TYPE: Description
+        """
 
         # SETUP TENSORBOARD FOR NEW RUN
-        checkpoint_prefix, run_dir = self._setup_tensorboard_and_log_new_run()
-        saver = tf.train.Saver(tf.global_variables())
+        if use_tensorboard:
+            checkpoint_prefix, run_dir = self._setup_tensorboard_directories()
+            saver = tf.train.Saver(tf.global_variables())
+        else:
+            self.logger.info('*** NEW RUN ***')
+        self._log_training_and_model_params()
         self.summary_op = tf.summary.merge_all()
 
         # TRAIN
         with tf.Session() as sess:
             # Initialization
-            progress_bar = progressbar.ProgressBar(max_value=self._train_length_ex)
+            progress_bar = progressbar.ProgressBar(max_value=self._train_length_steps)
             sess.run(tf.global_variables_initializer())
-            train_writer = tf.summary.FileWriter(run_dir + '/train', sess.graph)
-            val_writer = tf.summary.FileWriter(run_dir + '/val')
+            if use_tensorboard:
+                train_writer = tf.summary.FileWriter(run_dir + '/train', sess.graph)
+                val_writer = tf.summary.FileWriter(run_dir + '/val')
             batch_idx = 0
-            epoch_count = 0
             progress_bar.start()
             progress_bar.update(0)
 
@@ -104,20 +122,33 @@ class TrainModel(DataProcessor):
             try:
                 _, acc_test_before, _ = self.evaluate_model_on_data(sess, 'test')
                 for step in range(self._train_length_steps):
-                    if (step % ceil(self._train_length_steps / float(self.n_checks)) == 0) or \
-                       (step == self._train_length_steps - 1):
-                        # Check training and validation performance
-                        cost_train, acc_train, _ = self.evaluate_model_on_data(sess, 'train')
-                        cost_val, acc_val, summary = self.evaluate_model_on_data(sess, 'val')
+                    # Reset/increment batch_idx
+                    if step % self._steps_per_epoch == 0:
+                        batch_idx = 0
+                    else:
+                        batch_idx += 1
 
-                        # Report information to user
-                        self.logger.info('%d epochs elapsed.', epoch_count)
-                        self.logger.info('COST:     Train: %5.3f / Val: %5.3f', cost_train, cost_val)
-                        self.logger.info('ACCURACY: Train: %5.3f / Val: %5.3f', acc_train, acc_val)
+                    if use_tensorboard:
+                        do_full_eval = step % ceil(self._train_length_steps / float(self.n_checks)) == 0
+                        do_full_eval = do_full_eval or (step == self._train_length_steps - 1)
+                        if do_full_eval:
+                            # Check training and validation performance
+                            cost_train, acc_train, _ = self.evaluate_model_on_data(sess, 'train')
+                            cost_val, acc_val, summary = self.evaluate_model_on_data(sess, 'val')
 
-                        # Save to Tensorboard
-                        val_writer.add_summary(summary, step)
-                        saver.save(sess, checkpoint_prefix, global_step=step)
+                            # Report information to user
+                            self.logger.info('%d epochs elapsed.', step / self._steps_per_epoch)
+                            self.logger.info('COST:     Train: %5.3f / Val: %5.3f', cost_train, cost_val)
+                            self.logger.info('ACCURACY: Train: %5.3f / Val: %5.3f', acc_train, acc_val)
+
+                            # Save to Tensorboard
+                            val_writer.add_summary(summary, step)
+                            saver.save(sess, checkpoint_prefix, global_step=step)
+
+                            # # If model is not learning immediately, break out of training
+                            # if acc_val == acc_test_before and step > 100:
+                            #     self.logger.info('Stuck on value: %d', acc_val)
+                            #     break
 
                     # Training step
                     x_batch, y_batch = self._generate_batch(batch_idx)
@@ -126,17 +157,10 @@ class TrainModel(DataProcessor):
                                                      self.model.y: y_batch,
                                                      self.model.trainable: True})
 
-                    # Reset/incremenet batch_idx and epoch_count
-                    if step % self._steps_per_epoch == 0:
-                        batch_idx = 0
-                        epoch_count += 1
-                    else:
-                        batch_idx += 1
-
                     # Save to Tensorboard, update progress bar
-                    train_writer.add_summary(summary, step)
-                    progress = step * self.batch_size if step * self.batch_size < self._train_length_ex else self._train_length_ex
-                    progress_bar.update(progress)
+                    if use_tensorboard:
+                        train_writer.add_summary(summary, step)
+                    progress_bar.update(step)
             except KeyboardInterrupt:
                 self.logger.info('Keyboard Interrupt? Gracefully quitting.')
             finally:
@@ -145,8 +169,11 @@ class TrainModel(DataProcessor):
                 self.logger.info("The training is done.")
                 self.logger.info('Test accuracy before training: %.3f.', acc_test_before)
                 self.logger.info('Test accuracy after training: %.3f.', acc_test_after)
-                train_writer.close()
-                val_writer.close()
+                if use_tensorboard:
+                    train_writer.close()
+                    val_writer.close()
+
+        return acc_test_after
 
     def evaluate_model_on_data(self, sess, dataset_label):
         """Evaluate the model on the entire training data.
@@ -179,8 +206,8 @@ class TrainModel(DataProcessor):
         tf.reset_default_graph()
 
     """ Helper Functions """
-    def _setup_tensorboard_and_log_new_run(self):
-        """Set up TensorBoard directories and Record new run with important details
+    def _setup_tensorboard_directories(self):
+        """Set up TensorBoard directories.
 
         Returns:
             checkpoint_prefix, run_dir (string, string): checkpoint prefix, output root folder
@@ -198,16 +225,34 @@ class TrainModel(DataProcessor):
         # Logging the Run
         self.logger.info('*** NEW RUN ***')
         self.logger.info('filename: %s', model_name)
-        self.logger.info('  batch_size: %d', self.batch_size)
-        self.logger.info('  n_epochs: %d', self.n_epochs)
-        self.logger.info('  n_features: %d', self.n_features)
-        self.logger.info('  learning_rate: %f', self.learning_rate)
-        self.logger.info('  dropout_rate: %.2f', 0 if self.dropout_rate is None else self.dropout_rate)
 
         return checkpoint_prefix, run_dir
 
+    def _log_training_and_model_params(self):
+        """Record new run details."""
+        model_type = self.model.__class__.__name__
+
+        self.logger.info('  *** TRAINING ***')
+        self.logger.info('    n_epochs: %d', self.n_epochs)
+        self.logger.info('    batch_size: %d', self.batch_size)
+        self.logger.info('  *** MODEL ***')
+        if 'CNN' in model_type:
+            self.logger.info('    num_filt_1: %d', self.model.num_filt_1)
+            self.logger.info('    kernel_size: %d', self.model.kernel_size)
+            self.logger.info('    num_fc_1: %d', self.model.num_fc_1)
+        elif 'RNN' in model_type:
+            self.logger.info('    n_hidden: %d', self.model.n_hidden)
+            self.logger.info('    num_fc_1: %d', self.model.num_fc_1)
+            self.logger.info('    n_layers: %d', self.model.n_layers)
+
+        self.logger.info('    dropout_rate: %f', self.model.dropout_rate)
+        self.logger.info('    learning_rate: %f', self.model.learning_rate)
+        self.logger.info('    beta1: %f', self.model.beta1)
+        self.logger.info('    beta2: %f', self.model.beta2)
+        self.logger.info('    epsilon: %f', self.model.epsilon)
+
     def _generate_batch(self, batch_idx):
-        """Generate a batch and increment the sliding batch window within the data"""
+        """Generate a batch and increment the sliding batch window within the data."""
         features = self.train_data[0]
         labels = self.train_data[1]
 
@@ -232,33 +277,13 @@ class TrainModel(DataProcessor):
 
 def main():
     """Sup Main!"""
-    # CNN = TrainModel(CNNModel, n_epochs=200, batch_size=256, learning_rate=.00005, dropout_rate=0.5)
-    # CNN.train_model()
-    # CNN.reset_model()
-    # LSTM = TrainModel(LSTMModel, n_epochs=1000, batch_size=256, learning_rate=.0005, dropout_rate=0.5)
-    # LSTM.train_model()
-    # LSTM.reset_model()
+    models = [CNNModel(), RNNModel()]
+    for model in models:
+        model.build_model()
+        train = TrainModel(model, n_epochs=200, batch_size=128)
+        train.train_model()
+        train.reset_model()
 
-    CNN = TrainModel(CNNModel, n_epochs=1000, batch_size=256, learning_rate=.00005, dropout_rate=0.5)
-    CNN.train_model()
-    CNN.reset_model()
-    LSTM = TrainModel(LSTMModel, n_epochs=1000, batch_size=256, learning_rate=.001, dropout_rate=0.5)
-    LSTM.train_model()
-    LSTM.reset_model()
-
-    CNN = TrainModel(CNNModel, n_epochs=1000, batch_size=128, learning_rate=.00005, dropout_rate=0.5)
-    CNN.train_model()
-    CNN.reset_model()
-    LSTM = TrainModel(LSTMModel, n_epochs=1000, batch_size=128, learning_rate=.0005, dropout_rate=0.5)
-    LSTM.train_model()
-    LSTM.reset_model()
-
-    CNN = TrainModel(CNNModel, n_epochs=1000, batch_size=128, learning_rate=.00005, dropout_rate=0.5)
-    CNN.train_model()
-    CNN.reset_model()
-    LSTM = TrainModel(LSTMModel, n_epochs=1000, batch_size=128, learning_rate=.001, dropout_rate=0.5)
-    LSTM.train_model()
-    LSTM.reset_model()
 
 if __name__ == '__main__':
     # create logger with 'spam_application'
